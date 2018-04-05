72de89b4c5ff0b7121b4ff9a89771b801e064886
SANDEEP SANGOLE <sandeepsangole@garagescript.org>
2018-04-05 16:19:25 -0700

    Solves 2.js

--- curriculum/js1/2.js
+++ curriculum/js1/2.js
@@ -7,7 +7,14 @@
  */
 
 const solution = (a, b, c) => {
-  return Math.max(a,b,c);
+  if(a > b && a > c){
+    return a;
+  }
+  if(b > a && b > c){
+    return b;
+  }
+
+  return c;
 };
 
 module.exports = {
